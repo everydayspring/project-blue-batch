53d30396d94cc3fe4c523f64fa83726c243e7f52
everydayspring <68119202+everydayspring@users.noreply.github.com>
2024-11-08 11:43:45 +0900

    project init

---
+++ src/main/java/com/example/projectbluebatch/ProjectBlueBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectbluebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlueBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBlueBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectbluebatch/ProjectBlueBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbluebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlueBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
