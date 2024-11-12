a064969dc17890f4278e9efc06380222ff74152c
everydayspring <68119202+everydayspring@users.noreply.github.com>
2024-11-12 09:55:42 +0900

    [update] OldUsersAlertBatch - JDBC

--- src/main/java/com/example/projectbluebatch/batch/OldUsersAlertBatch.java
+++ src/main/java/com/example/projectbluebatch/batch/OldUsersAlertBatch.java
@@ -3,110 +3,73 @@ package com.example.projectbluebatch.batch;
 import com.example.projectbluebatch.config.JobTimeExecutionListener;
 import com.example.projectbluebatch.config.SlackNotifier;
 import com.example.projectbluebatch.entity.User;
-import com.example.projectbluebatch.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.data.RepositoryItemReader;
-import org.springframework.batch.item.data.RepositoryItemWriter;
-import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
-import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Sort;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Configuration
-@AllArgsConstructor
 public class OldUsersAlertBatch {
 
     private final JobRepository jobRepository;
-    private final PlatformTransactionManager platformTransactionManager;
     private final JobTimeExecutionListener jobTimeExecutionListener;
-
-    private final UserRepository userRepository;
     private final SlackNotifier slackNotifier;
+    private final JdbcTemplate jdbcTemplate;
 
-    private List<AlertUser> alertUsers;
+    public OldUsersAlertBatch(JobRepository jobRepository, JobTimeExecutionListener jobTimeExecutionListener, SlackNotifier slackNotifier, @Qualifier("dataDBSource") DataSource dataDBSource) {
+        this.jobRepository = jobRepository;
+        this.jobTimeExecutionListener = jobTimeExecutionListener;
+        this.slackNotifier = slackNotifier;
+        this.jdbcTemplate = new JdbcTemplate(dataDBSource);
+    }
 
     @Bean
     public Job oldUsersAlertBatchJob() {
-
-        alertUsers = new ArrayList<>();
-
         return new JobBuilder("oldUsersAlertBatchJob", jobRepository)
                 .start(oldUserAlertStep())
-                .next(oldUserAlertSlackStep())
                 .listener(jobTimeExecutionListener)
                 .build();
     }
 
     @Bean
     public Step oldUserAlertStep() {
-
         return new StepBuilder("oldUserAlertStep", jobRepository)
-                .<User, User>chunk(500, platformTransactionManager)
-                .reader(oldUserAlertReader())
-                .processor(oldUserAlertProcessor())
-                .writer(oldUserAlertWriter())
-                .build();
-    }
-
-    @Bean
-    public RepositoryItemReader<User> oldUserAlertReader() {
-
-        LocalDateTime targetDate = LocalDateTime.now().minusYears(2);
-
-        return new RepositoryItemReaderBuilder<User>()
-                .name("oldUserAlertReader")
-                .pageSize(50)
-                .methodName("findAllOldUser")
-                .arguments(targetDate)
-                .repository(userRepository)
-                .sorts(Map.of("id", Sort.Direction.ASC))
-                .build();
-    }
-
-    @Bean
-    public ItemProcessor<User, User> oldUserAlertProcessor() {
-
-        return user -> {
-            alertUsers.add(new AlertUser(user.getName(), user.getSlackId()));
-            return user;
-        };
-    }
-
-    @Bean
-    public RepositoryItemWriter<User> oldUserAlertWriter() {
-
-        return new RepositoryItemWriterBuilder<User>()
-                .repository(userRepository)
-                .methodName("save")
-                .build();
-    }
-
-    @Bean
-    public Step oldUserAlertSlackStep() {
-
-        return new StepBuilder("slackAlertStep", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
-                    alertUsers.forEach(alertUser -> {
+                    LocalDateTime targetDate = LocalDateTime.now().minusYears(2);
+
+                    String query = "SELECT * FROM users WHERE modified_at < ?";
+                    JdbcCursorItemReader<User> reader = new JdbcCursorItemReaderBuilder<User>()
+                            .dataSource(jdbcTemplate.getDataSource())
+                            .name("oldUserAlertReader") // 이름 추가
+                            .sql(query)
+                            .preparedStatementSetter((ps) -> ps.setObject(1, targetDate))
+                            .rowMapper((rs, rowNum) -> {
+                                User user = new User();
+                                user.setId(rs.getLong("id"));
+                                user.setName(rs.getString("name"));
+                                user.setSlackId(rs.getString("slack_id"));
+                                return user;
+                            })
+                            .build();
+
+                    reader.open(chunkContext.getStepContext().getStepExecution().getExecutionContext());
+
+                    User user;
+                    while ((user = reader.read()) != null) {
                         String title = "[비밀번호 변경 안내]";
-
-                        String userTag = "<@" + alertUser.getSlackId() + ">";
-
+                        String userTag = user.getSlackId() != null && !user.getSlackId().isEmpty() ? "<@" + user.getSlackId() + ">" : "";
                         String message = String.format(
                                 """
                                         %s \s
@@ -114,25 +77,14 @@ public class OldUsersAlertBatch {
                                         3년 이상 비밀번호를 변경하지 않는 경우 계정이 탈퇴됩니다.\s
                                         탈퇴 처리 이후 모든 예매내역 등은 복구할 수 없습니다.\s
                                         계정의 비밀번호를 변경해주시기 바랍니다.""",
-                                userTag, alertUser.getUsername()
+                                userTag, user.getName()
                         );
                         slackNotifier.sendMessage(title, message);
-                    });
+                    }
+
+                    reader.close();
                     return RepeatStatus.FINISHED;
                 }, new ResourcelessTransactionManager())
                 .build();
-    }
-}
-
-@Getter
-class AlertUser {
-
-    private final String username;
-    private final String slackId;
-
-    public AlertUser(String username, String slackId) {
-
-        this.username = username;
-        this.slackId = slackId;
     }
 }
